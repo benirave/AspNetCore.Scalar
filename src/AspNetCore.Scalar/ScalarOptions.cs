87c5fa1f035b22c38fae637b7d29fb356a01f3f2
Bence Eros <bence.eros@icloud.com>
2023-11-17 21:00:04 +0100

    Add ScalarOptions

---
+++ src/AspNetCore.Scalar/ScalarOptions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace AspNet.Scalar
+{
+    public class ScalarOptions
+    {
+        public string RoutePrefix { get; set; } = "scalar-api-docs";
+        
+        public string DocumentTitle { get; set; } = "API Docs";
+
+        public string HeadContent { get; set; } = "";
+
+        public string SpecUrl { get; set; } = "../swagger/v1/swagger.json";
+        
+        public Func<Stream> IndexStream { get; set; } = () => typeof(ScalarOptions).GetTypeInfo().Assembly 
+            .GetManifestResourceStream("AspNetCore.Scalar.index.html");
+        
+        public ConfigObject ConfigObject { get; set; } = new ConfigObject();
+    }
+
+    public class ConfigObject
+    {
+        public Theme Theme { get; set; } = Theme.Default;
+
+        public bool ShowSidebar { get; set; } = true;
+        
+        public char SearchHotKey { get; set; } = 'k';
+    }
+}
---
+++ src/AspNetCore.Scalar/Theme.cs
@@ -0,0 +1,12 @@
+﻿
+namespace AspNet.Scalar
+{
+    public enum Theme
+    {
+        Default = 0,
+        Alternate = 1,
+        Moon = 2,
+        Purple = 3,
+        Solarized = 4
+    }
+}
