7011c41a251bd1dd46923afbfcbfe636025f7bfc
Bence Eros <bence.eros@icloud.com>
2023-11-17 21:00:26 +0100

    Add Middleware

---
+++ src/AspNetCore.Scalar/ScalarMiddleware.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AspNetCore.Scalar;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+#if (NETSTANDARD2_0)
+using IWebHostEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+#endif
+
+namespace AspNet.Scalar
+{
+    public class ScalarMiddleware
+    {
+        private const string EmbeddedFileNamespace = "AspNetCore.Scalar.node_modules._scalar.api_reference.dist.browser";
+
+        private readonly ScalarOptions _options;
+        private readonly StaticFileMiddleware _staticFileMiddleware;
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+        public ScalarMiddleware(
+            RequestDelegate next,
+            IWebHostEnvironment hostingEnv,
+            ILoggerFactory loggerFactory,
+            ScalarOptions options)
+        {
+            _options = options ?? new ScalarOptions();
+
+            _staticFileMiddleware = CreateStaticFileMiddleware(next, hostingEnv, loggerFactory, options);
+
+            _jsonSerializerOptions = new JsonSerializerOptions();
+
+#if NET6_0
+            _jsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+#else
+            _jsonSerializerOptions.IgnoreNullValues = true;
+#endif
+            _jsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            _jsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, false));
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var httpMethod = httpContext.Request.Method;
+            var path = httpContext.Request.Path.Value;
+
+            if (httpMethod == "GET" && Regex.IsMatch(path, $"^/?{Regex.Escape(_options.RoutePrefix)}/?$",  RegexOptions.IgnoreCase))
+            {
+                var relativeIndexUrl = string.IsNullOrEmpty(path) || path.EndsWith("/")
+                    ? "index.html"
+                    : $"{path.Split('/').Last()}/index.html";
+
+                RespondWithRedirect(httpContext.Response, relativeIndexUrl);
+                return;
+            }
+
+            if (httpMethod == "GET" && Regex.IsMatch(path, $"/{_options.RoutePrefix}/?index.html",  RegexOptions.IgnoreCase))
+            {
+                await RespondWithIndexHtml(httpContext.Response);
+                return;
+            }
+
+            await _staticFileMiddleware.Invoke(httpContext);
+        }
+
+        private static StaticFileMiddleware CreateStaticFileMiddleware(
+            RequestDelegate next,
+            IWebHostEnvironment hostingEnv,
+            ILoggerFactory loggerFactory,
+            ScalarOptions options)
+        {
+            var staticFileOptions = new StaticFileOptions
+            {
+                RequestPath = string.IsNullOrEmpty(options.RoutePrefix) ? string.Empty : $"/{options.RoutePrefix}",
+                FileProvider = new EmbeddedFileProvider(typeof(ScalarOptions).GetTypeInfo().Assembly, EmbeddedFileNamespace),
+            };
+
+            return new StaticFileMiddleware(next, hostingEnv, Options.Create(staticFileOptions), loggerFactory);
+        }
+
+        private void RespondWithRedirect(HttpResponse response, string location)
+        {
+            response.StatusCode = 301;
+            response.Headers["Location"] = location;
+        }
+
+        private async Task RespondWithIndexHtml(HttpResponse response)
+        {
+            response.StatusCode = 200;
+            response.ContentType = "text/html";
+
+            using (var stream = _options.IndexStream())
+            {
+                var htmlBuilder = new StringBuilder(await new StreamReader(stream).ReadToEndAsync());
+                foreach (var entry in GetIndexArguments())
+                {
+                    htmlBuilder.Replace(entry.Key, entry.Value);
+                }
+
+                await response.WriteAsync(htmlBuilder.ToString(), Encoding.UTF8);
+            }
+        }
+
+        private IDictionary<string, string> GetIndexArguments()
+        {
+            return new Dictionary<string, string>
+            {
+                { "%(DocumentTitle)", _options.DocumentTitle },
+                { "%(HeadContent)", _options.HeadContent },
+                { "%(SpecUrl)", _options.SpecUrl },
+                { "%(ConfigObject)", JsonSerializer.Serialize(_options.ConfigObject, _jsonSerializerOptions) }
+            };
+        }
+    }
+}
