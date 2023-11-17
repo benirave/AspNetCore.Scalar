9aaa8db271a94bb7ee4ccb2442210053b91b20f1
Bence Eros <bence.eros@icloud.com>
2023-11-17 21:00:39 +0100

    Add BuilderExtensions

---
+++ src/AspNetCore.Scalar/ScalarBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace AspNet.Scalar
+{
+    public static class ScalarBuilderExtensions
+    {
+        public static IApplicationBuilder UseScalar(this IApplicationBuilder app, ScalarOptions options)
+        {
+            return app.UseMiddleware<ScalarMiddleware>(options);
+        }
+        
+        public static IApplicationBuilder UseScalar(
+            this IApplicationBuilder app,
+            Action<ScalarOptions> setupAction = null)
+        {
+            ScalarOptions options;
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                options = scope.ServiceProvider.GetRequiredService<IOptionsSnapshot<ScalarOptions>>().Value;
+                setupAction?.Invoke(options);
+            }
+            
+            return app.UseScalar(options);
+        }
+    }
+}
