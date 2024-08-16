4827abf3da2a357b4f88868670f5906ce56cea7f
Bence Eros <bence.eros@icloud.com>
2024-08-16 02:34:41 +0200

    add .net 5

--- tests/AspNetCore.Scalar.Integration.Tests/ScalarUITests.cs
+++ tests/AspNetCore.Scalar.Integration.Tests/ScalarUITests.cs
@@ -4,53 +4,73 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
 
-namespace AspNetCore.Scalar.Integration.Tests;
-
-public class ScalarUITests
+namespace AspNetCore.Scalar.Integration.Tests
 {
-    private const string ScalarUIPath = "scalar-api-docs";
 
-    [Fact]
-    public async Task UseScalar_ShouldRenderScalarUI()
+    public class ScalarUITests
     {
-        // Arrange
-        using var client = BuildServer().CreateClient();
+        private const string ScalarUIPath = "scalar-api-docs";
 
-        // Act
-        var healthResponse = await client.GetAsync("health");
-        var scalarResponse = await client.GetAsync($"{ScalarUIPath}/index.html");
+        [Fact]
+        public async Task UseScalar_ShouldRenderScalarUI()
+        {
+            // Arrange
+            using var client = BuildServer().CreateClient();
 
-        // Assert
-        Assert.True(healthResponse.IsSuccessStatusCode, "Api did not start");
-        Assert.True(scalarResponse.IsSuccessStatusCode, "Scalar UI did not render");
-    }
+            // Act
+            var healthResponse = await client.GetAsync("health");
+            var scalarResponse = await client.GetAsync($"{ScalarUIPath}/index.html");
 
-    private static TestServer BuildServer()
-    {
-        var builder = new WebHostBuilder()
-            .ConfigureServices(services =>
-            {
-                services.AddEndpointsApiExplorer();
-                services.AddRouting();
-                services.AddSwaggerGen();
-            })
-            .Configure(app =>
-            {
-                app.UseRouting();
-                app.UseSwagger();
-                app.UseScalar();
+            // Assert
+            Assert.True(healthResponse.IsSuccessStatusCode, "Api did not start");
+            Assert.True(scalarResponse.IsSuccessStatusCode, "Scalar UI did not render");
+        }
 
-                app.UseEndpoints(endpointBuilder =>
+        private static TestServer BuildServer()
+        {
+            var builder = new WebHostBuilder()
+                .ConfigureServices(services =>
                 {
-                    endpointBuilder.MapGet("/health", () =>
-                    {
-                        return "OK";
-                    });
-                });
-            })
-            .UseTestServer();
+#if NET6_0_OR_GREATER
+   
+                    services.AddEndpointsApiExplorer();
+#endif
+#if NET5_0
+                    services.AddMvcCore()
+                            .AddApiExplorer();
+#endif
+                    services.AddRouting();
+                    services.AddSwaggerGen();
+                })
+                .Configure(app =>
+                {
+                    app.UseRouting();
+                    app.UseSwagger();
+                    app.UseScalar();
 
-        return new TestServer(builder);
+                    app.UseEndpoints(endpointBuilder =>
+                    {
+#if NET6_0_OR_GREATER
+                        endpointBuilder.MapGet("/health", () =>
+                        {
+
+                            return "OK";
+                        });
+#endif
+#if NET5_0
+                        endpointBuilder.MapGet("/health", _ =>
+                        {
+
+                            return Task.FromResult("OK");
+                        });
+#endif
+                    });
+                })
+                .UseTestServer();
+
+            return new TestServer(builder);
+        }
     }
 }
