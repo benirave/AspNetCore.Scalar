ab28db574afdd94c679c7cab4452c7636e3af9a9
Bence Eros <bence.eros@icloud.com>
2024-09-10 18:12:46 +0200

    Add rough solution with angelsharp

--- tests/AspNetCore.Scalar.Integration.Tests/ScalarUITests.cs
+++ tests/AspNetCore.Scalar.Integration.Tests/ScalarUITests.cs
@@ -5,6 +5,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using System.Text.Json;
+using AngleSharp;
+using System;
+using System.Linq;
+
+
+
+
 #if NET5_0
 using Microsoft.AspNetCore.Mvc;
 #endif
@@ -16,6 +23,8 @@ namespace AspNetCore.Scalar.Integration.Tests
     {
         private const string ScalarUIPath = "scalar-api-docs";
         private const string DefaultSwaggerDocPath = "swagger/v1/swagger.json";
+
+        private static IBrowsingContext HtmlParser = BrowsingContext.New(Configuration.Default);
 
         [Fact]
         public async Task UseScalar_ShouldRenderScalarUI()
@@ -30,13 +39,36 @@ namespace AspNetCore.Scalar.Integration.Tests
 
             // Assert
             Assert.True(healthResponse.IsSuccessStatusCode, "Api did not start");
-
             Assert.True(swaggerDocResponse.IsSuccessStatusCode, "Swagger doc did not load");
-            Assert.True(DoesEndpointExistsInSwaggerJson(await swaggerDocResponse.Content.ReadAsStringAsync()), "Endpoint does not exists in swagger json");
-
             Assert.True(scalarResponse.IsSuccessStatusCode, "Scalar UI did not render");
 
+            var swaggerJson = await swaggerDocResponse.Content.ReadAsStringAsync();
+            Assert.True(DoesEndpointExistsInSwaggerJson(swaggerJson), "Endpoint does not exists in swagger json");
 
+            var scalarHtml = await scalarResponse.Content.ReadAsStringAsync();
+
+            var document = await HtmlParser.OpenAsync(req => req.Content(scalarHtml));
+            var apiReference = document.GetElementById("api-reference");
+            var dataUrl = apiReference.GetAttribute("data-url");
+
+
+            Assert.True(dataUrl.Equals("../swagger/v1/swagger.json", StringComparison.InvariantCultureIgnoreCase), "Data url is incorrect.");
+            var configuration = apiReference.GetAttribute("data-configuration");
+
+            var scalarConfiguration = JsonSerializer.Deserialize<ConfigObject>(configuration);
+            Assert.NotNull(scalarConfiguration);
+            Assert.Equal(Theme.Default, scalarConfiguration.Theme);
+            Assert.Equal(Layout.Modern, scalarConfiguration.Layout);
+            Assert.Equal(true, scalarConfiguration.ShowSidebar);
+            Assert.Equal('k', scalarConfiguration.SearchHotKey);
+
+            Assert.NotNull(scalarConfiguration.AdditionalItems);
+
+            var scriptTags = document.GetElementsByTagName("script");
+            var foo = scriptTags.Select(x => x.GetAttribute("src")).Where(x => !string.IsNullOrEmpty(x));
+
+            Assert.Contains("standalone.js", foo);
+            Assert.True(foo.Count() == 1);
             // Verify here that the initial html has the good swagger data-url and the configuration is set correctly
 
             // and test that the UI and everything is working with Playwright as an E2E test solution?
