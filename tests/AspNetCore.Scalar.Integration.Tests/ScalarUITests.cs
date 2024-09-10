5e8a302a6b462978cf53f9fc26d94d58ed170b42
Bence Eros <bence.eros@icloud.com>
2024-09-10 17:30:11 +0200

    Extending test

--- tests/AspNetCore.Scalar.Integration.Tests/ScalarUITests.cs
+++ tests/AspNetCore.Scalar.Integration.Tests/ScalarUITests.cs
@@ -4,7 +4,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+#if NET5_0
 using Microsoft.AspNetCore.Mvc;
+#endif
 
 namespace AspNetCore.Scalar.Integration.Tests
 {
@@ -12,6 +15,7 @@ namespace AspNetCore.Scalar.Integration.Tests
     public class ScalarUITests
     {
         private const string ScalarUIPath = "scalar-api-docs";
+        private const string DefaultSwaggerDocPath = "swagger/v1/swagger.json";
 
         [Fact]
         public async Task UseScalar_ShouldRenderScalarUI()
@@ -20,12 +24,30 @@ namespace AspNetCore.Scalar.Integration.Tests
             using var client = BuildServer().CreateClient();
 
             // Act
+            var swaggerDocResponse = await client.GetAsync(DefaultSwaggerDocPath);
             var healthResponse = await client.GetAsync("health");
             var scalarResponse = await client.GetAsync($"{ScalarUIPath}/index.html");
 
             // Assert
             Assert.True(healthResponse.IsSuccessStatusCode, "Api did not start");
+
+            Assert.True(swaggerDocResponse.IsSuccessStatusCode, "Swagger doc did not load");
+            Assert.True(DoesEndpointExistsInSwaggerJson(await swaggerDocResponse.Content.ReadAsStringAsync()), "Endpoint does not exists in swagger json");
+
             Assert.True(scalarResponse.IsSuccessStatusCode, "Scalar UI did not render");
+
+
+            // Verify here that the initial html has the good swagger data-url and the configuration is set correctly
+
+            // and test that the UI and everything is working with Playwright as an E2E test solution?
+        }
+
+        private static bool DoesEndpointExistsInSwaggerJson(string swaggerJson)
+        {
+            var doc = JsonSerializer.Deserialize<JsonElement>(swaggerJson);
+
+            var path = doc.GetProperty("paths");
+            return path.TryGetProperty("/health", out _);
         }
 
         private static TestServer BuildServer()
@@ -52,18 +74,14 @@ namespace AspNetCore.Scalar.Integration.Tests
 
                     app.UseEndpoints(endpointBuilder =>
                     {
+#if NET5_0
+                        endpointBuilder.MapControllers();
+#endif
 #if NET6_0_OR_GREATER
                         endpointBuilder.MapGet("/health", () =>
                         {
 
                             return "OK";
-                        });
-#endif
-#if NET5_0
-                        endpointBuilder.MapGet("/health", _ =>
-                        {
-
-                            return Task.FromResult("OK");
                         });
 #endif
                     });
@@ -74,3 +92,16 @@ namespace AspNetCore.Scalar.Integration.Tests
         }
     }
 }
+
+#if NET5_0
+
+[ApiController]
+public class TestController
+{
+    [HttpGet("/health")]
+    public IActionResult Health()
+    {
+        return new OkResult();
+    }
+}
+#endif
