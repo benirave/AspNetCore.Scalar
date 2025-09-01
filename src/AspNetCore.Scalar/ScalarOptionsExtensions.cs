53ebcf1cb9919d92e39d915b5ecabb4b80665a36
Bence Eros <benirave@gmail.com>
2025-09-01 18:59:32 +0200

    Add CDN support

--- src/AspNetCore.Scalar/ScalarOptionsExtensions.cs
+++ src/AspNetCore.Scalar/ScalarOptionsExtensions.cs
@@ -4,11 +4,19 @@ namespace AspNetCore.Scalar
 {
     public static class ScalarOptionsExtensions
     {
+        private const string DefaultCdnUrl = "https://cdn.jsdelivr.net/npm/@scalar/api-reference";
+
+        
         public static void InjectStylesheet(this ScalarOptions options, string path, string media = "screen")
         {
             var builder = new StringBuilder(options.HeadContent);
             builder.AppendLine($"<link href='{path}' rel='stylesheet' media='{media}' type='text/css' />");
             options.HeadContent = builder.ToString();
+        }
+        
+        public static void UseCdn(this ScalarOptions options, string url = DefaultCdnUrl)
+        {
+            options.CdnUrl = url;
         }
 
         public static void UseSpecUrl(this ScalarOptions options, string url)
