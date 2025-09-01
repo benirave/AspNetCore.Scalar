ef1830864fbecd5c888e8f86e61f8082d7ee5d60
Bence Eros <benirave@gmail.com>
2025-09-01 18:35:35 +0200

    Update scalar to latest version

--- example/WebApi/Program.cs
+++ example/WebApi/Program.cs
@@ -19,8 +19,6 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.UseHttpsRedirection();
-
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
